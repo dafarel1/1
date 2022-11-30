50f493371c45bc9858bb51408f24f740d85c92b2
dafarel1 <dafareliqbal123@gmail.com>
2022-11-30 08:42:42 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,79 @@
+/**
+   * Create By Dika Ardnt.
+   * Recode By Naze Dev && Papah-Chan
+   * Contact Me on wa.me/628989513496
+   * Follow https://github.com/FahriAdison
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://api.lolhuman.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://api.lolhuman.xyz': '5c0144af2b3cc9cef3d5d504',
+}
+
+// Other
+global.owner = ['6289603658404']
+global.ownernomer = "6289603658404"
+global.premium = ['6289603658404']
+global.packname = 'Dapa By'
+global.author = 'Dapa'
+global.sessionName = 'papah'  //jangan diganti bro nanti error
+global.jumlha = '999'
+global.jumhal = '100000000000000'
+global.jumlah = '1000000000'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+
+// Setting Mess
+global.mess = {
+    success: '✅Selesai',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    error: 'Error!',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+
+// Limit
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
+
+// Fake
+global.thumb = fs.readFileSync('./media/image/papah.jpg')
+global.faall = fs.readFileSync('./media/image/menu.jpg')
+
+// Url
+global.mygit = 'https://github.com/Fan'
+global.myyt = 'https://bit.ly/Chan'
+global.myytv = ''
+global.mygc = ""
+
+// Engak Ngaruh, Belum Kepasang
+global.botname = 'Dapa BOT'
+global.akulaku = 'Dapa Bot By Dapa'
+global.ytname = 'gada' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
